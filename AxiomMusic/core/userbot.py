fa0add2851ce8d10d9b8a6b7f318d61dd086ca33
The Maanav <prettylioness0550@gmail.com>
2026-04-20 20:17:43 +0530

    ㅤ Аксиом Ботс ㅤ

--- AxiomMusic/core/userbot.py
+++ AxiomMusic/core/userbot.py
@@ -27,35 +27,35 @@ class Userbot(Client):
             name="AxiommXAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            _string=str(config.STRING1),
+            session_string=str(config.STRING1),
             no_updates=True,
         )
         self.two = Client(
             name="AxiommXAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            _string=str(config.STRING2),
+            session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
             name="AxiommXAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            _string=str(config.STRING3),
+            session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
             name="AxiommXAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            _string=str(config.STRING4),
+            session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
             name="AxiommXAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
-            _string=str(config.STRING5),
+            session_string=str(config.STRING5),
             no_updates=True,
         )
 
