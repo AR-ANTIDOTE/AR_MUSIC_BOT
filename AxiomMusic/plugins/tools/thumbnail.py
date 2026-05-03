086391f2bb5198fb100de9a8c734547ac3cff604
The Maanav <prettylioness0550@gmail.com>
2026-05-03 14:02:12 +0530

    ㅤ Аксиом Ботс ㅤ

---
+++ AxiomMusic/plugins/tools/thumbnail.py
@@ -0,0 +1,45 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from AxiomMusic import app
+from AxiomMusic.utils.database import (
+    thumbnail_on,
+    thumbnail_off,
+    is_thumbnail,
+)
+
+
+@app.on_message(filters.command("thumbnail") & filters.group)
+async def thumbnail_cmd(_, message: Message):
+    if len(message.command) < 2:
+        status = await is_thumbnail(message.chat.id)
+
+        return await message.reply_text(
+            f"<b>𝚻ʜ꧊‌𝛖ϻβηᴧιℓ 𝛅𝞃ᴧ𝞃𝛖s:</b> {'Enabled' if status else 'Disabled'}\n\n"
+            "Usᴧɢє:\n"
+            "/thumbnail on\n"
+            "/thumbnail off"
+        )
+
+    option = message.command[1].lower()
+
+    if option == "on":
+        await thumbnail_on(message.chat.id)
+
+        return await message.reply_text(
+            "<b>𝚻ʜ꧊‌𝛖ϻβηᴧιℓ 𝚺ηᴧβℓє∂.</b>"
+        )
+
+    elif option == "off":
+        await thumbnail_off(message.chat.id)
+
+        return await message.reply_text(
+            "<b>𝚻ʜ꧊‌𝛖ϻβηᴧιℓ ∂ιsᴧβℓє∂.</b>"
+        )
+
+    else:
+        return await message.reply_text(
+            "Usᴧɢє:\n"
+            "/thumbnail on\n"
+            "/thumbnail off"
+        )
