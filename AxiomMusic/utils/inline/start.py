f3c7b6098a41c5f9a5b16557b0990f9f3f83af99
segcoder1 <segfaulted01@gmail.com>
2026-05-30 14:52:21 +0530

    Update start.py

--- AxiomMusic/utils/inline/start.py
+++ AxiomMusic/utils/inline/start.py
@@ -24,13 +24,13 @@ def start_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?startgroup=true", style=Buttonstyle.PRIMARY,
+                url=f"https://t.me/{app.username}?startgroup=true", style=ButtonStyle.PRIMARY,
             )
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_2"],
-                url=config.SUPPORT_CHANNEL, style=Buttonstyle.PRIMARY,
+                url=config.SUPPORT_CHANNEL, style=ButtonStyle.PRIMARY,
             ),
         ],
     ]
@@ -42,27 +42,27 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true", style=Buttonstyle.PRIMARY,
+                url=f"https://t.me/{app.username}?startgroup=true", style=ButtonStyle.PRIMARY,
             ),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=Buttonstyle.PRIMARY),
-            InlineKeyboardButton(text="📼ʏᴛ-ᴀᴘɪ", callback_data="api_status", style=Buttonstyle.PRIMARY),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="📼ʏᴛ-ᴀᴘɪ", callback_data="api_status", style=ButtonStyle.PRIMARY),
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_6"],
-                url=config.SUPPORT_CHAT, style=Buttonstyle.PRIMARY,
+                url=config.SUPPORT_CHAT, style=ButtonStyle.PRIMARY,
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"],
-                url=config.SUPPORT_CHANNEL, style=Buttonstyle.PRIMARY,
+                url=config.SUPPORT_CHANNEL, style=ButtonStyle.PRIMARY,
             ),
         ],
         [
             InlineKeyboardButton(
                 text=_["S_B_4"],
-                callback_data="settings_back_helper", style=Buttonstyle.PRIMARY,
+                callback_data="settings_back_helper", style=ButtonStyle.PRIMARY,
             ),
         ],
     ]
