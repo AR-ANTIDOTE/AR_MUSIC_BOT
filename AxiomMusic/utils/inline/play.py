791801779f0357dd248ff439b3fe14e0d8853c33
The Maanav <prettylioness0550@gmail.com>
2026-05-31 17:23:53 +0530

    Update play.py

--- AxiomMusic/utils/inline/play.py
+++ AxiomMusic/utils/inline/play.py
@@ -85,20 +85,18 @@ def stream_markup_timer(_, chat_id, played, dur):
         )
     ],
     [
-        InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-        InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-        InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"), 
-        InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"), 
-        InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}", style=ButtonStyle.DANGER),
+        InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}", style=ButtonStyle.DANGER),
+        InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}", style=ButtonStyle.DANGER), 
+        InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.DANGER),
     ],
     [
-        InlineKeyboardButton(text=thumb_text, callback_data=f"THUMBTOGGLE|{chat_id}")
+        InlineKeyboardButton(text=thumb_text, callback_data=f"THUMBTOGGLE|{chat_id}", style=ButtonStyle.PRIMARY),
     ],  # ← ADD COMMA HERE
     [
         InlineKeyboardButton("⪻ -𝟸5s", callback_data="seek_backward_20", style=ButtonStyle.PRIMARY), 
+        InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}", style=ButtonStyle.DANGER), 
         InlineKeyboardButton("+𝟸5s ⪼", callback_data="seek_forward_20", style=ButtonStyle.PRIMARY),
-    ],
-    [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
